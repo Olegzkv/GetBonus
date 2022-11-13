3709c2f023277d98e2ab1e161b19103d13a17bd7
Oleg <zkv_testpost@mail.ru>
2022-11-13 11:50:03 +0300

    GetBonus

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int adding = 2000;
         int minimal = 100;
 
-        if (adding >= 1000) {
+        if (adding > 1000) {
             System.out.println("Баланс=" + (currentBalance + adding + (adding / minimal)));
         } else {
             System.out.println("Баланс=" + (adding + currentBalance));
